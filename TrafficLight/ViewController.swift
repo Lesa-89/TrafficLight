c8b047f09987e283d3a672a21c757359f9629aa5
Larichev Sergey <lesa-89@mail.ru>
2019-07-25 00:08:14 +0300

    New release

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var switchButton: UIButton!
+    
+    var activeColor = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        redLight.backgroundColor = .red
+        redLight.alpha = 0.3
+        
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        yellowLight.backgroundColor = .yellow
+        yellowLight.alpha = 0.3
+        
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        greenLight.backgroundColor = .green
+        greenLight.alpha = 0.3
+        
     }
-
-
+    
+    @IBAction func switchLightButton(_ sender: Any) {
+        switch activeColor {
+        case "Red":
+            activeColor = "Yellow"
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case "Yellow":
+            activeColor = "Green"
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        case "Green":
+            activeColor = "Red"
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        default:
+            activeColor = "Red"
+            redLight.alpha = 1
+            switchButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
